cd307dc5811f56d1bc9491b49e5935ea502a344d
karl ducta <karlducta022@gmail.com>
2024-03-01 22:44:57 +0800

    Creating the Customer Slice

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -1,8 +1,44 @@
-const initialStateCustomer = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   fullName: "",
   nationalID: "",
   createdAt: "",
 };
+
+const customerSlice = createSlice({
+  name: "customer",
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        return {
+          payload: {
+            fullName,
+            nationalID,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
+
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = new Date().toISOString();
+      },
+    },
+
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
+
+export const { createCustomer, updateName } = customerSlice.actions;
+
+export default customerSlice.reducer;
+
+/*
 
 export default function customerReducer(state = initialStateCustomer, action) {
   switch (action.type) {
@@ -43,3 +79,4 @@ function updateName(fullName) {
 }
 
 export { createCustomer, updateName };
+*/
