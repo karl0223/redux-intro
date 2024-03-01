3018dd4adc1c358a17f47e8f4111a7fc93a049f6
karl ducta <karlducta022@gmail.com>
2024-03-01 14:16:09 +0800

    Redux: Creating a Reducer --- Bank Account

---
+++ src/store.js
@@ -0,0 +1,34 @@
+const initialSate = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+function reducer(state = initialSate, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+      };
+    case "account/withdraw":
+      return {
+        ...state,
+        balance: state.balance - action.payload,
+      };
+    case "account/requestLoan":
+      return {
+        ...state,
+        loan: action.payload,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+  }
+}
